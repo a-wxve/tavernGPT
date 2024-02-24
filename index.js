947bdec70881d15899f89d1cfcc9615d385dc17f
a-wxve <117374906+a-wxve@users.noreply.github.com>
2024-02-24 14:41:51 -0600

    Fix jank
    Formatting cleanup, add transitions, yada yada

--- index.js
+++ index.js
@@ -1,62 +1,79 @@
-import { eventSource, displayPastChats, getUserAvatar, getRequestHeaders, name1, getThumbnailUrl, getEntitiesList, generateQuietPrompt, characters, this_chid, callPopup } from '../../../../script.js';
-import { getContext } from '../../../extensions.js';
-import { getGroupAvatar, groups, renameGroupChat, selected_group } from '../../../group-chats.js';
-import { delay } from '../../../utils.js';
+import {
+    callPopup,
+    characters,
+    displayPastChats,
+    eventSource,
+    generateQuietPrompt,
+    getEntitiesList,
+    getRequestHeaders,
+    getThumbnailUrl,
+    getUserAvatar,
+    name1,
+    this_chid,
+} from "../../../../script.js";
+import { getContext } from "../../../extensions.js";
+import {
+    getGroupAvatar,
+    groups,
+    renameGroupChat,
+    selected_group,
+} from "../../../group-chats.js";
+import { delay } from "../../../utils.js";
 
-const extensionName = 'Extension-TavernGPT';
+const extensionName = "Extension-TavernGPT";
 const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
 
 async function loadFavorites() {
     const favHTML = await $.get(`${extensionFolderPath}/favorites.html`);
-    $('#top-settings-holder').append(favHTML);
+    $("#top-settings-holder").append(favHTML);
 
     const entities = getEntitiesList({ doFilter: false });
-    const container = $('#top-settings-holder .hotswap2');
-    const template = $('#hotswap_template2 .hotswapAvatar');
+    const container = $("#top-settings-holder .hotswap2");
+    const template = $("#hotswap_template2 .hotswapAvatar");
 
     const promises = [];
     const newContainer = container.clone();
     newContainer.empty();
 
     for (const entity of entities) {
-        const isFavorite = entity.item.fav || entity.item.fav == 'true';
+        const isFavorite = entity.item.fav || entity.item.fav == "true";
 
         if (!isFavorite) {
             continue;
         }
 
-        const isCharacter = entity.type === 'character';
-        const isGroup = entity.type === 'group';
+        const isCharacter = entity.type === "character";
+        const isGroup = entity.type === "group";
 
-        const grid = isGroup ? entity.id : '';
-        const chid = isCharacter ? entity.id : '';
+        const grid = isGroup ? entity.id : "";
+        const chid = isCharacter ? entity.id : "";
 
         let slot = template.clone();
-        slot.toggleClass('character_select', isCharacter);
-        slot.toggleClass('group_select', isGroup);
-        slot.attr('grid', isGroup ? grid : '');
-        slot.attr('chid', isCharacter ? chid : '');
-        slot.data('id', isGroup ? grid : chid);
+        slot.toggleClass("character_select", isCharacter);
+        slot.toggleClass("group_select", isGroup);
+        slot.attr("grid", isGroup ? grid : "");
+        slot.attr("chid", isCharacter ? chid : "");
+        slot.data("id", isGroup ? grid : chid);
 
         if (isGroup) {
-            const group = groups.find(x => x.id === grid);
+            const group = groups.find((x) => x.id === grid);
             const avatar = getGroupAvatar(group);
-            $(slot).find('img').replaceWith(avatar);
-            $(slot).attr('title', group.name);
+            $(slot).find("img").replaceWith(avatar);
+            $(slot).attr("title", group.name);
         }
 
         if (isCharacter) {
             const imgLoadPromise = new Promise((resolve) => {
-                const avatarUrl = getThumbnailUrl('avatar', entity.item.avatar);
-                $(slot).find('img').attr('src', avatarUrl).on('load', resolve);
-                $(slot).attr('title', entity.item.avatar);
+                const avatarUrl = getThumbnailUrl("avatar", entity.item.avatar);
+                $(slot).find("img").attr("src", avatarUrl).on("load", resolve);
+                $(slot).attr("title", entity.item.avatar);
             });
 
             // if the image doesn't load in 500ms, resolve the promise anyway
             promises.push(Promise.race([imgLoadPromise, delay(500)]));
         }
 
-        $(slot).css('cursor', 'pointer');
+        $(slot).css("cursor", "pointer");
         newContainer.append(slot);
     }
 
@@ -75,10 +92,10 @@ async function loadHistory() {
         }
 
         if (matchesTimePattern(old_filename) && context.chat.length !== 1) {
-            const prompt = 'Generate a name for this chat in less than 6 words. Avoid including non-alphanumeric characters, words like "chat", or the user\'s name.';
+            const prompt =
+                'Generate a name for this chat in less than 6 words. Avoid including non-alphanumeric characters, words like "chat", or the user\'s name.';
             let newName = await generateQuietPrompt(prompt, false, false);
-            newName = newName.toString().replace(/^"((?:\\"|[^"])*)"$/, '$1');
-
+            newName = newName.toString().replace(/^"((?:\\"|[^"])*)"$/, "$1");
 
             const body = {
                 is_group: !!selected_group,
@@ -87,75 +104,82 @@ async function loadHistory() {
                 renamed_file: `${newName}.jsonl`,
             };
 
-
             try {
-                const response = await fetch('/api/chats/rename', {
-                    method: 'POST',
+                const response = await fetch("/api/chats/rename", {
+                    method: "POST",
                     body: JSON.stringify(body),
                     headers: getRequestHeaders(),
                 });
 
                 if (!response.ok) {
-                    throw new Error('Unsuccessful request.');
+                    throw new Error("Unsuccessful request.");
                 }
 
                 const data = await response.json();
 
                 if (data.error) {
-                    throw new Error('Server returned an error.');
+                    throw new Error("Server returned an error.");
                 }
 
                 if (selected_group) {
-                    await renameGroupChat(selected_group, old_filename, newName);
-                }
-                else {
+                    await renameGroupChat(
+                        selected_group,
+                        old_filename,
+                        newName,
+                    );
+                } else {
                     if (characters[this_chid].chat == old_filename) {
                         characters[this_chid].chat = newName;
                     }
-                    $('#selected_chat_pole').val(characters[this_chid].chat);
+                    $("#selected_chat_pole").val(characters[this_chid].chat);
                 }
 
                 await delay(250);
-                $('#option_select_chat').trigger('click');
-                $('#options').hide();
+                $("#option_select_chat").trigger("click");
+                $("#options").hide();
             } catch {
                 await delay(500);
-                await callPopup('An error has occurred. Chat was not renamed.', 'text');
+                await callPopup(
+                    "An error has occurred. Chat was not renamed.",
+                    "text",
+                );
             }
         }
     }
 
     const historyHTML = await $.get(`${extensionFolderPath}/history.html`);
-    $('#top-settings-holder').append(historyHTML);
+    $("#top-settings-holder").append(historyHTML);
 
-    $('#shadow_select_chat_popup').remove();
+    $("#shadow_select_chat_popup").remove();
 
-    $('.options-content #option_select_chat, .options-content #option_start_new_chat, .options-content #option_close_chat').css('display', 'none');
+    $(
+        ".options-content #option_select_chat, .options-content #option_start_new_chat, .options-content #option_close_chat",
+    ).css("display", "none");
 
-    $('#new_chat').on('click', function() {
-        $('.options-content #option_start_new_chat').trigger('click');
+    $("#new_chat").on("click", function () {
+        $(".options-content #option_start_new_chat").trigger("click");
     });
-    $('#close_chat').on('click', function() {
-        $('.options-content #option_close_chat').trigger('click');
+    $("#close_chat").on("click", function () {
+        $(".options-content #option_close_chat").trigger("click");
     });
 
-    eventSource.on('chatLoaded', async () => {
+    eventSource.on("chatLoaded", async () => {
         await displayPastChats();
         await renameChat();
     });
 }
 
 async function getPersona() {
-    const response = await fetch('/api/settings/get', {
-        method: 'POST',
+    const response = await fetch("/api/settings/get", {
+        method: "POST",
         headers: getRequestHeaders(),
         body: JSON.stringify({}),
-        cache: 'no-cache',
+        cache: "no-cache",
     });
 
     if (!response.ok) {
-        toastr.error('Settings could not be loaded. Try reloading the page.');
-        throw new Error('Error getting settings');
+        toastr.error("Settings could not be loaded. Try reloading the page.");
+        throw new Error("Error getting settings");
     }
 
     const data = await response.json();
@@ -163,27 +187,36 @@ async function getPersona() {
 
     let user_avatar = settings.user_avatar;
     let avatar_img = getUserAvatar(user_avatar);
-    $('#persona-management-button .drawer-icon.fa-solid.fa-face-smile').html(`<img class='persona_avatar' src='${avatar_img}'/>`);
-    $('#persona-management-button .drawer-icon.fa-solid.fa-face-smile').append(`<span> ${name1}</span>`);
+    $("#persona-management-button .drawer-icon.fa-solid.fa-face-smile").html(
+        `<img class='persona_avatar' src='${avatar_img}'/>`,
+    );
+    $("#persona-management-button .drawer-icon.fa-solid.fa-face-smile").append(
+        `<span> ${name1}</span>`,
+    );
 
-    $('#persona-management-button .drawer-icon.fa-solid.fa-face-smile > *').on('click', function () {
-        $(this).closest('.drawer').find('.drawer-content').toggleClass('closedDrawer openDrawer');
-    });
+    $("#persona-management-button .drawer-icon.fa-solid.fa-face-smile > *").on(
+        "click",
+        function () {
+            $(this)
+                .closest(".drawer")
+                .find(".drawer-content")
+                .toggleClass("closedDrawer openDrawer");
+        },
+    );
 }
 
 function closeSidebar() {
     const closeHTML = `<button class="closeSidebar">
-                            <div class="arrow1"></div>
-                            <div class="arrow2"></div>
-                        </button>`;
-    $('#top-settings-holder').append(closeHTML);
+                              <div class="arrow1"></div>
+                              <div class="arrow2"></div>
+                          </button>`;
+    $("#top-settings-holder").append(closeHTML);
 
-    $('.closeSidebar').on('click', function() {
-        if (!$('#top-settings-holder').hasClass('collapsed')) {
-            $('#top-settings-holder, #sheld').addClass('collapsed');
-        }
-        else {
-            $('#top-settings-holder, #sheld').removeClass('collapsed');
+    $(".closeSidebar").on("click", function () {
+        if (!$("#top-settings-holder").hasClass("collapsed")) {
+            $("#top-settings-holder, #sheld").addClass("collapsed");
+        } else {
+            $("#top-settings-holder, #sheld").removeClass("collapsed");
         }
     });
 }
